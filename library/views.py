69a2fc6f8d58ea4d0b0137020f5c09ffc26a5e41
dmtrDO <dmytro.domaratskiy@gmail.com>
2024-12-27 00:50:50 +0200

    fixing small details in views's funtions

--- library/views.py
+++ library/views.py
@@ -1,5 +1,5 @@
 
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from .models import (BookCard,AuthorBookCard, Author, Book,
                      Genre, Account, Librarian, Reader)
 from .forms import BookForm, LoginForm, RegisterForm
@@ -22,6 +22,50 @@ def get_books_list():
             'release_year': book_card.release_year,
         })
     return book_list
+
+def librarian_add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            genre_title = form.cleaned_data['genre']
+            isbn = form.cleaned_data['isbn']
+            author_full_name = form.cleaned_data['author']
+            release_year = form.cleaned_data['release_year']
+            is_in_reading_room = form.cleaned_data['is_in_reading_room']
+            inventory_number = form.cleaned_data['inventory_number']
+
+            genre, _ = Genre.objects.get_or_create(
+                title=genre_title
+            )
+            book_card, _ = BookCard.objects.get_or_create(
+                genre=genre,
+                title=title,
+                isbn=isbn,
+                release_year=release_year)
+            author, _ = Author.objects.get_or_create(
+                full_name=author_full_name
+            )
+            AuthorBookCard.objects.get_or_create(
+                book_card=book_card,
+                author=author
+            )
+            Book.objects.create(
+                book_card=book_card,
+                in_reading_room=is_in_reading_room,
+                inventory_number=inventory_number
+            )
+            return redirect('./')
+        else:
+            return render(request, 'lib_add_book.html',
+                          {'form': form, 'errors': 'incorrect values'})
+    form = BookForm()
+    return render(request, 'lib_add_book.html',
+                  {'form': form})
+
+def home(request):
+    return render(request, 'index.html',
+                  {'books': get_books_list()})
 
 def register(request):
     if request.method == 'POST':
@@ -81,10 +125,6 @@ def login(request):
     }
     return render(request, "login.html", data)
 
-def home(request):
-    return render(request, 'index.html',
-                  {'books': get_books_list()})
-
 def librarian(request):
     return render(request, 'librarian.html',
                   {'books': get_books_list()})
@@ -92,45 +132,4 @@ def librarian(request):
 def reader(request):
     return render(request, 'reader.html',
                   {'books': get_books_list()})
-
-def librarian_add_book(request):
-    if request.method == 'POST':
-        form = BookForm(request.POST)
-        if form.is_valid():
-            title = form.cleaned_data['title']
-            genre_title = form.cleaned_data['genre']
-            isbn = form.cleaned_data['isbn']
-            author_full_name = form.cleaned_data['author']
-            release_year = form.cleaned_data['release_year']
-            is_in_reading_room = form.cleaned_data['is_in_reading_room']
-            inventory_number = form.cleaned_data['inventory_number']
-
-            genre, _ = Genre.objects.get_or_create(
-                title=genre_title
-            )
-            book_card, _ = BookCard.objects.get_or_create(
-                genre=genre,
-                title=title,
-                isbn=isbn,
-                release_year=release_year)
-            author, _ = Author.objects.get_or_create(
-                full_name=author_full_name
-            )
-            AuthorBookCard.objects.get_or_create(
-                book_card=book_card,
-                author=author
-            )
-            Book.objects.create(
-                book_card=book_card,
-                in_reading_room=is_in_reading_room,
-                inventory_number=inventory_number
-            )
-            return redirect('./')
-        else:
-            return render(request, 'lib_add_book.html',
-                          {'form': form, 'errors': 'incorrect values'})
-    form = BookForm()
-    return render(request, 'lib_add_book.html',
-                  {'form': form})
-
 
